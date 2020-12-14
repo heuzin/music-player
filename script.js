c76fc0c6b4567efcca98e0fa210b5fbbd15e0a78
heuzin <matheusqs.arq@gmail.com>
2020-12-14 11:23:57 -0500

    updating progress bar

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const artist = document.getElementById('artist');
 const music = document.querySelector('audio');
 const progressContainer = document.getElementById('progress-container');
 const progress = document.getElementById('progress');
+const currentTimeEl = document.getElementById('current-time');
+const durationEl = document.getElementById('duration');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
@@ -94,7 +96,24 @@ function updateProgressBar(e) {
         const { duration, currentTime } = e.srcElement;
         // Update progress bar widrh
         const progressPercent = (currentTime / duration) * 100;
-        progress.style.width = `${progressPercent}%`
+        progress.style.width = `${progressPercent}%`;
+        // Calculate display for duration
+        const durationMinutes = Math.floor(duration / 60);
+        let durationSeconds = Math.floor(duration % 60)
+        if (durationSeconds < 10) {
+            durationSeconds = `0${durationSeconds}`;
+        }
+        // Delay switching the duration Element to avoid NaN
+        if (durationMinutes) {
+            durationEl.textContent = `${durationMinutes}:${durationSeconds}`;
+        }
+        // Calculate display for current
+        const currentMinutes = Math.floor(currentTime / 60);
+        let currentSeconds = Math.floor(currentTime % 60)
+        if (currentSeconds < 10) {
+            currentSeconds = `0${currentSeconds}`;
+        }
+        currentTimeEl.textContent = `${currentMinutes}:${currentSeconds}`
     }
 }
 
